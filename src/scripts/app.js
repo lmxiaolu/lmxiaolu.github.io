4dab154d98d3d193409d0664111840f2c3696a57
1573129761 <1573129761@qq.com>
2016-06-04 13:16:51 +0800

    lm

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -2,12 +2,11 @@
 
 require('./lib/spa.min.js');
 require('./lib/swiper-3.3.1.min.js');
-require('./views/guide.js');
-require('./views/index.js');
-require('./views/diff.js');
-require('./views/home.js'); 
+require('/views/guide.js');
+require('/views/index.js');
+require('/views/diff.js');
+require('/views/home.js'); 
 // 配置视图的信息
 SPA.config({
-  indexView: 'guide' 
+  indexView: 'guide'
 });
- 
